82d02b1f53bb97bc36300ced688482b4a336d773
Jagannath <jagannathdebgunin@gmail.com>
2025-04-06 19:40:51 +0530

    Your commit message describing the change

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,7 @@
 const About=()=>(
     <section className="section">
         <h2>About me</h2>
-        <p>I am a web</p>
+        <p>I am a web developer and push this to github</p>
     </section>
 );
 export default About;
